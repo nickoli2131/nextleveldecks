63f66a2cd98611b37a192c8b7e4a8aed2bc8512b
nickoli2131 <nickoli2131@gmail.com>
2026-03-14 15:05:59 -0700

    switched ' to " on base line

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/nextleveldecks/' : '/',
+  base: mode === "production" ? "/nextleveldecks/" : "/",
+ // base: "/nextleveldecks/",
   server: {
     host: "::",
     port: 8080,
