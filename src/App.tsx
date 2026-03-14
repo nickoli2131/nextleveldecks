7d5225f3307a9d3309aa7376eba1c79563108024
nickoli2131 <nickoli2131@gmail.com>
2026-03-14 15:20:55 -0700

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/nextleveldecks">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
